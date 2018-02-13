9a3c015b1410dc25afac6f27ae0899b1bcb8a671
Damien Villeneuve <d.villeneuve@beapp.fr>
2018-02-13 19:14:35 +0100

    Allow EnumUtils.fromName to return null value in case of fail matching

--- utils/src/main/java/fr/beapp/utils/enums/EnumUtils.java
+++ utils/src/main/java/fr/beapp/utils/enums/EnumUtils.java
@@ -11,6 +11,27 @@ public class EnumUtils {
 	/**
 	 * Retrieve an {@link Enum} value according to the given key to look for.
 	 *
+	 * @param items a set of {@link Enum} values to look through
+	 * @param key   the key to search in items
+	 * @param <E>   the type of the Enum value to search, which must implement {@link EnumFromWS}
+	 * @return the item value if found, <code>null</code> otherwise
+	 */
+	@Nullable
+	static <E extends EnumFromWS> E fromKey(@NonNull E[] items, @Nullable String key) {
+		if (key == null)
+			return null;
+
+		for (E item : items) {
+			if (key.equalsIgnoreCase(item.getKey()))
+				return item;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Retrieve an {@link Enum} value according to the given key to look for.
+	 *
 	 * @param items        a set of {@link Enum} values to look through
 	 * @param key          the key to search in items
 	 * @param defaultValue the value to return if no match was found
@@ -28,6 +49,27 @@ public class EnumUtils {
 		}
 
 		return defaultValue;
+	}
+
+	/**
+	 * Retrieve an {@link Enum} value according to his name.
+	 *
+	 * @param clazz The class of {@link Enum} to search names into
+	 * @param name  the {@link Enum} name to search in items
+	 * @param <E>   the type of the Enum value to search
+	 * @return the item value if found, <code>null</code> otherwise
+	 */
+	@Nullable
+	public static <E extends Enum<E>> E fromName(@NonNull Class<E> clazz, @Nullable String name) {
+		if (name == null)
+			return null;
+
+		try {
+			return Enum.valueOf(clazz, name);
+		} catch (Exception ignored) {
+			// Nothing to do here. The enum was simply not found
+		}
+		return null;
 	}
 
 	/**
