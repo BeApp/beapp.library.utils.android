fef083c50880e89fcb7b5ba35cf0194901a72e49
Damien Villeneuve <d.villeneuve@beapp.fr>
2018-02-14 22:31:31 +0100

    Add KeyboardUtility

---
+++ utils/src/main/java/fr/beapp/utils/android/KeyboardUtility.java
@@ -0,0 +1,36 @@
+package fr.beapp.utils.android;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class KeyboardUtility {
+	private KeyboardUtility() {
+	}
+
+	/**
+	 * Set focus on the given {@link View} and display keyboard
+	 *
+	 * @param view view to gain focus
+	 */
+	public static void showKeyboard(@NonNull View view) {
+		view.requestFocus();
+		InputMethodManager inputMethodManager = (InputMethodManager) view.getContext().getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (inputMethodManager != null) {
+			inputMethodManager.showSoftInput(view, 0);
+		}
+	}
+
+	/**
+	 * Hide keyboard which was shown from the given {@link View}
+	 *
+	 * @param view the view which gained focus
+	 */
+	public static void hideKeyboard(@NonNull View view) {
+		InputMethodManager inputMethodManager = (InputMethodManager) view.getContext().getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (inputMethodManager != null) {
+			inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+		}
+	}
+}
