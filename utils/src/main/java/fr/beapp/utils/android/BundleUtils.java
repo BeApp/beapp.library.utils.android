b32c70e8ae29ebd3e40fed274eef8138a2f4d660
Damien Villeneuve <d.villeneuve@beapp.fr>
2018-02-15 22:15:19 +0100

    Add unit tests on BundleUtils

--- utils/src/main/java/fr/beapp/utils/android/BundleUtils.java
+++ utils/src/main/java/fr/beapp/utils/android/BundleUtils.java
@@ -14,120 +14,124 @@ public class BundleUtils {
 	private BundleUtils() {
 	}
 
+	static boolean matchedType(@NonNull Bundle bundle, @NonNull String key, @NonNull Class<?> clazz) {
+		Object value = bundle.get(key);
+		return value != null && clazz.isAssignableFrom(value.getClass());
+	}
+
 	// BOOLEAN
 
 	@Nullable
-	public static Boolean getBoolean(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getBoolean(key) : null;
+	public static Boolean getBoolean(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Boolean.class) ? bundle.getBoolean(key) : null;
 	}
 
-	public static boolean getBoolean(@Nullable Bundle extras, @NonNull String key, boolean defaultValue) {
-		return extras != null ? extras.getBoolean(key, defaultValue) : defaultValue;
+	public static boolean getBoolean(@Nullable Bundle bundle, @NonNull String key, boolean defaultValue) {
+		return bundle != null ? bundle.getBoolean(key, defaultValue) : defaultValue;
 	}
 
 	// BYTE
 
 	@Nullable
-	public static Byte getByte(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getByte(key) : null;
+	public static Byte getByte(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Byte.class) ? bundle.getByte(key) : null;
 	}
 
-	public static byte getByte(@Nullable Bundle extras, @NonNull String key, byte defaultValue) {
-		return extras != null ? extras.getByte(key, defaultValue) : defaultValue;
+	public static byte getByte(@Nullable Bundle bundle, @NonNull String key, byte defaultValue) {
+		return bundle != null ? bundle.getByte(key, defaultValue) : defaultValue;
 	}
 
 	// CHAR
 
-	public static Character getChar(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getChar(key) : null;
+	@Nullable
+	public static Character getChar(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Character.class) ? bundle.getChar(key) : null;
 	}
 
-	public static char getChar(@Nullable Bundle extras, @NonNull String key, char defaultValue) {
-		return extras != null ? extras.getChar(key, defaultValue) : defaultValue;
+	public static char getChar(@Nullable Bundle bundle, @NonNull String key, char defaultValue) {
+		return bundle != null ? bundle.getChar(key, defaultValue) : defaultValue;
 	}
 
 	// SHORT
 
 	@Nullable
-	public static Short getShort(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getShort(key) : null;
+	public static Short getShort(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Short.class) ? bundle.getShort(key) : null;
 	}
 
-	public static short getShort(@Nullable Bundle extras, @NonNull String key, short defaultValue) {
-		return extras != null ? extras.getShort(key, defaultValue) : defaultValue;
+	public static short getShort(@Nullable Bundle bundle, @NonNull String key, short defaultValue) {
+		return bundle != null ? bundle.getShort(key, defaultValue) : defaultValue;
 	}
 
 	// INT
 
-
 	@Nullable
-	public static Integer getInt(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getInt(key) : null;
+	public static Integer getInt(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Integer.class) ? bundle.getInt(key) : null;
 	}
 
-	public static int getInt(@Nullable Bundle extras, @NonNull String key, int defaultValue) {
-		return extras != null ? extras.getInt(key, defaultValue) : defaultValue;
+	public static int getInt(@Nullable Bundle bundle, @NonNull String key, int defaultValue) {
+		return bundle != null ? bundle.getInt(key, defaultValue) : defaultValue;
 	}
 
 	// LONG
 
-
 	@Nullable
-	public static Long getLong(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getLong(key) : null;
+	public static Long getLong(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Long.class) ? bundle.getLong(key) : null;
 	}
 
-	public static long getLong(@Nullable Bundle extras, @NonNull String key, long defaultValue) {
-		return extras != null ? extras.getLong(key, defaultValue) : defaultValue;
+	public static long getLong(@Nullable Bundle bundle, @NonNull String key, long defaultValue) {
+		return bundle != null ? bundle.getLong(key, defaultValue) : defaultValue;
 	}
 
 	// FLOAT
 
 	@Nullable
-	public static Float getFloat(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getFloat(key) : null;
+	public static Float getFloat(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Float.class) ? bundle.getFloat(key) : null;
 	}
 
 
-	public static float getFloat(@Nullable Bundle extras, @NonNull String key, float defaultValue) {
-		return extras != null ? extras.getFloat(key, defaultValue) : defaultValue;
+	public static float getFloat(@Nullable Bundle bundle, @NonNull String key, float defaultValue) {
+		return bundle != null ? bundle.getFloat(key, defaultValue) : defaultValue;
 	}
 
 	// DOUBLE
 
 
 	@Nullable
-	public static Double getDouble(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getDouble(key) : null;
+	public static Double getDouble(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, Double.class) ? bundle.getDouble(key) : null;
 	}
 
 
-	public static double getDouble(@Nullable Bundle extras, @NonNull String key, double defaultValue) {
-		return extras != null ? extras.getDouble(key, defaultValue) : defaultValue;
+	public static double getDouble(@Nullable Bundle bundle, @NonNull String key, double defaultValue) {
+		return bundle != null ? bundle.getDouble(key, defaultValue) : defaultValue;
 	}
 
 	// STRING
 
 	@Nullable
-	public static String getString(@Nullable Bundle extras, @NonNull String key) {
-		return extras != null && extras.containsKey(key) ? extras.getString(key) : null;
+	public static String getString(@Nullable Bundle bundle, @NonNull String key) {
+		return bundle != null && matchedType(bundle, key, String.class) ? bundle.getString(key) : null;
 	}
 
 	@NonNull
-	public static String getString(@Nullable Bundle extras, @NonNull String key, @NonNull String defaultValue) {
-		return extras != null ? extras.getString(key, defaultValue) : defaultValue;
+	public static String getString(@Nullable Bundle bundle, @NonNull String key, @NonNull String defaultValue) {
+		return bundle != null ? bundle.getString(key, defaultValue) : defaultValue;
 	}
 
 	// SERIALIZABLE
 
 	@Nullable
 	@SuppressWarnings("unchecked")
-	public static <E extends Serializable> E getSerializable(@Nullable Bundle extras, @NonNull String key) {
-		if (extras == null) {
+	public static <E extends Serializable> E getSerializable(@Nullable Bundle bundle, @NonNull String key) {
+		if (bundle == null) {
 			return null;
 		}
 
-		Serializable value = extras.getSerializable(key);
+		Serializable value = bundle.getSerializable(key);
 		try {
 			return (E) value;
 		} catch (ClassCastException e) {
@@ -137,8 +141,8 @@ public class BundleUtils {
 	}
 
 	@NonNull
-	public static <E extends Serializable> E getSerializable(@Nullable Bundle extras, @NonNull String key, E defaultValue) {
-		E value = getSerializable(extras, key);
+	public static <E extends Serializable> E getSerializable(@Nullable Bundle bundle, @NonNull String key, E defaultValue) {
+		E value = getSerializable(bundle, key);
 		return value != null ? value : defaultValue;
 	}
 
@@ -146,12 +150,12 @@ public class BundleUtils {
 
 	@Nullable
 	@SuppressWarnings("unchecked")
-	public static <E extends Parcelable> E getParcelable(@Nullable Bundle extras, @NonNull String key) {
-		if (extras == null) {
+	public static <E extends Parcelable> E getParcelable(@Nullable Bundle bundle, @NonNull String key) {
+		if (bundle == null) {
 			return null;
 		}
 
-		Parcelable value = extras.getParcelable(key);
+		Parcelable value = bundle.getParcelable(key);
 		try {
 			return (E) value;
 		} catch (ClassCastException e) {
@@ -161,8 +165,8 @@ public class BundleUtils {
 	}
 
 	@NonNull
-	public static <E extends Parcelable> E getParcelable(@Nullable Bundle extras, @NonNull String key, E defaultValue) {
-		E value = getParcelable(extras, key);
+	public static <E extends Parcelable> E getParcelable(@Nullable Bundle bundle, @NonNull String key, E defaultValue) {
+		E value = getParcelable(bundle, key);
 		return value != null ? value : defaultValue;
 	}
 
