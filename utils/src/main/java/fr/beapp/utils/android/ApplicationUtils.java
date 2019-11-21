045f9c5bdde2ebee371ca21b39ed953fecce0156
Florentin Noel <f.noel@beapp.fr>
2019-11-21 18:24:43 +0100

    Added replacement for deprecated getVersionCode

--- utils/src/main/java/fr/beapp/utils/android/ApplicationUtils.java
+++ utils/src/main/java/fr/beapp/utils/android/ApplicationUtils.java
@@ -2,9 +2,12 @@ package fr.beapp.utils.android;
 
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import androidx.annotation.RequiresApi;
 import fr.beapp.logger.Logger;
 
 public class ApplicationUtils {
@@ -17,10 +20,28 @@ public class ApplicationUtils {
 	 *
 	 * @param context the calling context
 	 * @return the application's version code, or null if: context is null or package name couldn't be resolved
+	 *
+	 * @deprecated Use {@link #getLongVersionCode(Context)} instead
 	 */
 	public static int getVersionCode(@NonNull Context context) {
 		try {
 			return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+		} catch (NameNotFoundException e) {
+			Logger.error("Can't retrieve application's version code", e);
+		}
+		return -1;
+	}
+
+	/**
+	 * Retrieve the application's version code for platform SDK > 28
+	 *
+	 * @param context the calling context
+	 * @return the application's version code, or null if: context is null or package name couldn't be resolved
+	 */
+	@RequiresApi(api = Build.VERSION_CODES.P)
+	public static long getLongVersionCode(@NonNull Context context) {
+		try {
+			return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).getLongVersionCode();
 		} catch (NameNotFoundException e) {
 			Logger.error("Can't retrieve application's version code", e);
 		}
